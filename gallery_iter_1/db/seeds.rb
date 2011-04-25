c4a5306b37f9f8e5a9e7a6562856ede3f0e94082
almartin <alex.m.martineau@gmail.com>
2011-04-25 19:12:07 -0400

    Removing old files.

--- gallery_iter_1/db/seeds.rb
+++
@@ -1,4 +0,0 @@
-require 'csv'
-CSV.foreach "lib/Art Collection.csv" do |row|
-  Artwork.create(:filename => row[0], :object => row[1], :creator => row[2], :title => row[3], :description => row[4], :location => row[5], :recfrom => row[6], :date => row[7])
-end
