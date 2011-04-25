28f412acf003daf94c34fa6f0a2f8eb176870e00
unknown <Hairic@.(none)>
2011-04-25 18:46:57 -0400

    with aws

--- gallery_iter_1/app/controllers/collections_controller.rb
+++ gallery_iter_1/app/controllers/collections_controller.rb
@@ -1,16 +1,55 @@
+require 'aws/s3'
+
+class ArtworkDescriptor
+	def initialize(title, creator, date, object, description, recfrom, location, imglink)
+		@title = title
+		@creator = creator
+		@date = date
+		@object = object
+		@description = description
+		@recfrom = recfrom
+		@location = location
+		@imglink = imglink
+	end
+	
+	attr_accessor :title, :creator, :date, :object, :description, :recfrom, :location, :imglink
+end
+
 class CollectionsController < ApplicationController
 	def index
 		@type = params[:type]
 		@value = params[:value]
+		
 		case @type
 		when 'artist'
-			@artworks = Artwork.where("creator = ?", params[:value])
+			dbartworks = Artwork.where("creator = ?", params[:value])
 		when 'donor'
-			@artworks = Artwork.where("recfrom = ?", params[:value])
+			dbartworks = Artwork.where("recfrom = ?", params[:value])
 		when 'location'
-			@artworks = Artwork.where("location = ?", params[:value])
+			dbartworks = Artwork.where("location = ?", params[:value])
 		else
 			redirect_to ''
 		end
+
+		@artworks = []
+		dbartworks.each do |artwork|
+			AWS::S3::Base.establish_connection!(
+				:access_key_id     => '',
+				:secret_access_key => ''
+			)
+			imglink = (AWS::S3::S3Object.url_for(artwork.filename + '.jpg','Art_Images'))
+			@artworks.push(ArtworkDescriptor.new(artwork.title,
+												artwork.creator,
+												artwork.date,
+												artwork.object,
+												artwork.description,
+												artwork.recfrom,
+												artwork.location,
+												imglink))
+		end
+	end
+	
+	def artists
+		
 	end
 end
