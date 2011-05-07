5023f64e1cebf5db4bae4601893d08682bab413a
akhaku01 <ammar.khaku@tufts.edu>
2011-05-06 23:53:41 -0400

    finishing merging cms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110505042412) do
+ActiveRecord::Schema.define(:version => 20110507035051) do
 
   create_table "artworks", :force => true do |t|
     t.string   "object"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110505042412) do
     t.string   "thumbnail"
     t.float    "lat"
     t.float    "lng"
+    t.boolean  "issuper"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -65,18 +66,18 @@ ActiveRecord::Schema.define(:version => 20110505042412) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "",    :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "issuper",                             :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
